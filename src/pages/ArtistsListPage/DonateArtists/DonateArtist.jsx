20831f41d1a98547d0b4e345d7b6ff0e2cfb97e2
k-jipgeria <sgim43547@gmail.com>
2024-10-26 22:21:57 +0900

    feat: Add temporary DonationArtist component for displaying the donation section / 후원 섹션을 보여주기 위한 임시 DonationArtist 컴포넌트 추가

---
+++ src/pages/ArtistsListPage/DonateArtists/DonateArtist.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import SpreadCards from './Components/SpreadCards';
+
+function DonateArtists() {
+  return (
+    <>
+      <SpreadCards />
+    </>
+  );
+}
+
+export default DonateArtists;
