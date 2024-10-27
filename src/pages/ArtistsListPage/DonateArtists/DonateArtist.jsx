324b9e6bc97d8a545e9da15ffc77db166ae84f5f
k-jipgeria <sgim43547@gmail.com>
2024-10-27 10:07:25 +0900

    refactor: Pagination component 호출을 SpreadCards.jsx => DonateArtist.jsx로 이동

--- src/pages/ArtistsListPage/DonateArtists/DonateArtist.jsx
+++ src/pages/ArtistsListPage/DonateArtists/DonateArtist.jsx
@@ -1,11 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import SpreadCards from './Components/SpreadCards';
+import Pagination from './Components/Pagination';
+import mockData from 'utils/mockData';
 
 function DonateArtists() {
+  const itemsPerPage = 4;
+  const dataLength = mockData.length;
+
+  // Pagination 관련 상태를 상위 컴포넌트에서 관리
+  const [startIndex, setStartIndex] = useState(0);
+
   return (
-    <>
-      <SpreadCards />
-    </>
+    <Pagination dataLength={dataLength} setStartIndex={setStartIndex} itemsPerPage={itemsPerPage}>
+      <SpreadCards startIndex={startIndex} itemsPerPage={itemsPerPage} />
+    </Pagination>
   );
 }
 
