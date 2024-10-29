307b9137e9e7b7101937385110504939ee60c403
lemon1335 <kimjinhui114@gmail.com>
2024-10-29 21:57:23 +0900

    [style] change common-component

--- src/components/favoriteArtists/FavoriteArtists.jsx
+++ src/components/favoriteArtists/FavoriteArtists.jsx
@@ -1,11 +1,11 @@
-import ProfileBadge from 'components/common/ProfileBadge';
+import ProfileDelete from 'components/common/ProfileDelete';
 
 function FavoriteArtists() {
   return (
     <div className="favorite-artist-container">
       <div className="favorite-artist-title">내가 관심있는 아이돌</div>
       <div className="favorite-artist">
-        <ProfileBadge img="img_idol_example.svg" size="medium" className="favorite-artist-badge" />
+        <ProfileDelete img="img_idol_example.svg" size="medium" />
         <div className="artist-info">
           <div className="artist-name">민지</div>
           <div className="artist-group">뉴진스</div>
