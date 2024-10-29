80c404b6e4c9bbcdcb9d9446cac4f527b36ce782
lemon1335 <kimjinhui114@gmail.com>
2024-10-29 15:07:47 +0900

    [refactor] add type="button" on profile-button

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,11 +1,11 @@
-import { useNavigate } from "react-router-dom";
-import parseImg from "utils/images";
+import {useNavigate} from 'react-router-dom';
+import parseImg from 'utils/images';
 
 function Nav() {
   const navigate = useNavigate();
 
   const handleNavigateMypage = () => {
-    navigate("/myPage");
+    navigate('/myPage');
   };
 
   const clickLogo = () => {
@@ -15,21 +15,11 @@ function Nav() {
 
   return (
     <div className="nav">
-      <button
-        type="button"
-        onClick={clickLogo} /*클릭시 새로고침 페이지로 넘어가기*/
-        className="nav-logo-btn"
-      >
-        <img src={parseImg("img_logo.svg")} alt="로고" />
+      <button type="button" onClick={clickLogo} /*클릭시 새로고침 페이지로 넘어가기*/ className="nav-logo-btn">
+        <img src={parseImg('img_logo.svg')} alt="로고" />
       </button>
-      <button  onClick={
-          handleNavigateMypage
-        } /*클릭시 /mypage로 이동, 프로필 이미지는 자유*/ className="moving-mypage-button">
-      <img
-        src={parseImg("img_userProfile.svg")}
-        alt="내프로필사진"
-        className="nav-profile-img"
-      />
+      <button type="button" onClick={handleNavigateMypage} /*클릭시 /mypage로 이동, 프로필 이미지는 자유*/ className="moving-mypage-button">
+        <img src={parseImg('img_userProfile.svg')} alt="내프로필사진" className="nav-profile-img" />
       </button>
     </div>
   );
