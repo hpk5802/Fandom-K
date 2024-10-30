f387a9a7809b2ba19fccea35f7e81577876d90c3
lemon1335 <kimjinhui114@gmail.com>
2024-10-30 23:43:40 +0900

    [style] add container for grid & favoriteArtists-component design-ui done

--- src/components/favoriteArtists/FavoriteArtists.jsx
+++ src/components/favoriteArtists/FavoriteArtists.jsx
@@ -1,14 +1,16 @@
 import ProfileDelete from 'components/common/ProfileDelete';
 
-function FavoriteArtists() {
+function FavoriteArtists({handleDelete}) {
   return (
-    <div className="favorite-artist-container">
+    <div className="entire-favorite-artist">
       <div className="favorite-artist-title" /*로컬 스토리지 관리부분*/>내가 관심있는 아이돌</div>
-      <div className="favorite-artist">
-        <ProfileDelete img="img_idol_example.svg" size="medium" /*x버튼 누르면 삭제 */ />
-        <div className="artist-info">
-          <div className="artist-name">민지</div>
-          <div className="artist-group">뉴진스</div>
+      <div className="favorite-artists-container">
+        <div className="favorite-artist">
+          <ProfileDelete img="img_idol_example.svg" size="medium" onClick={handleDelete} /*x버튼 누르면 삭제 */ />
+          <div className="artist-info">
+            <div className="artist-name">민지</div>
+            <div className="artist-group">뉴진스</div>
+          </div>
         </div>
       </div>
     </div>
