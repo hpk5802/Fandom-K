74283e94529500e154c269f7484864802482408f
ksm <hpk5802@github.com>
2024-10-30 14:10:43 +0900

    [refactor][강수민] : 후원하기 컴포넌트 구조 수정

--- src/components/donateArtist/Pagination.jsx
+++ src/components/donateArtist/Pagination.jsx
@@ -1,20 +1,32 @@
-import React from 'react';
 import parseImg from 'utils/images';
 
 const arrowLeft = parseImg('ic_pagination_arrow_left.svg');
 const arrowRight = parseImg('ic_pagination_arrow_right.svg');
 
-function Pagination({onNext, onPrevious, canGoNext, canGoPrevious, children}) {
+function Pagination({cursor, currentPage, totalPages, onPageChange, fetchMoreDonations, children}) {
+  const canGoNext = cursor || currentPage + 1 < totalPages;
+  const canGoPrev = currentPage > 0;
+
+  const handleNextPage = () => {
+    if (currentPage + 1 >= totalPages) fetchMoreDonations();
+    onPageChange(currentPage + 1);
+  };
+
+  const handlePreviousPage = () => {
+    if (currentPage > 0) {
+      onPageChange(currentPage - 1);
+    }
+  };
   return (
     <div className="pagination-container">
-      {canGoPrevious && (
-        <button type="button" className="pagination-icon prev" onClick={onPrevious}>
+      {canGoPrev && (
+        <button type="button" className="pagination-icon prev" onClick={handlePreviousPage}>
           <img src={arrowLeft} alt="" />
         </button>
       )}
       {children}
       {canGoNext && (
-        <button type="button" className="pagination-icon next" onClick={onNext}>
+        <button type="button" className="pagination-icon next" onClick={handleNextPage}>
           <img src={arrowRight} alt="" />
         </button>
       )}
