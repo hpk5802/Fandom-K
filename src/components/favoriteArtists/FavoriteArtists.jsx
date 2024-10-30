5404740ee5fec1daaa1a74cdfb874c340bd60f12
oms <po1202222@gmail.com>
2024-10-30 17:10:33 +0900

    [fix][오명섭] : lint 미적용된 컴포넌트 수정

--- src/components/favoriteArtists/FavoriteArtists.jsx
+++ src/components/favoriteArtists/FavoriteArtists.jsx
@@ -5,7 +5,7 @@ function FavoriteArtists() {
     <div className="favorite-artist-container">
       <div className="favorite-artist-title" /*로컬 스토리지 관리부분*/>내가 관심있는 아이돌</div>
       <div className="favorite-artist">
-        <ProfileDelete img="img_idol_example.svg" size="medium" /*x버튼 누르면 삭제 *//>
+        <ProfileDelete img="img_idol_example.svg" size="medium" /*x버튼 누르면 삭제 */ />
         <div className="artist-info">
           <div className="artist-name">민지</div>
           <div className="artist-group">뉴진스</div>
--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -15,16 +15,16 @@ function Nav() {
 
   return (
     <div className="nav">
-      <div className='button-containter'>
-      <button type="button" onClick={clickLogo} /*클릭시 새로고침 페이지로 넘어가기*/ className="nav-logo-btn">
-        <img src={parseImg('img_logo.svg')} alt="로고" />
-      </button>
-      <button type="button" onClick={handleNavigateMypage} /*클릭시 /mypage로 이동, 프로필 이미지는 자유*/ className="moving-mypage-button">
-        <img src={parseImg('img_userProfile.svg')} alt="내프로필사진" className="nav-profile-img" />
-      </button>
+      <div className="button-containter">
+        <button type="button" onClick={clickLogo} /*클릭시 새로고침 페이지로 넘어가기*/ className="nav-logo-btn">
+          <img src={parseImg('img_logo.svg')} alt="로고" />
+        </button>
+        <button type="button" onClick={handleNavigateMypage} /*클릭시 /mypage로 이동, 프로필 이미지는 자유*/ className="moving-mypage-button">
+          <img src={parseImg('img_userProfile.svg')} alt="내프로필사진" className="nav-profile-img" />
+        </button>
       </div>
     </div>
-  ); 
+  );
 }
 
 export default Nav;
