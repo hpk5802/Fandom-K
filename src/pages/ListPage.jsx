4376c8ecbab51b3f419b5612487decda49667cad
ksm <hpk5802@github.com>
2024-10-30 14:19:23 +0900

    [feat][강수민] : ListPage 데이터 연동한 컴포넌트 추가

--- src/pages/ListPage.jsx
+++ src/pages/ListPage.jsx
@@ -1,10 +1,17 @@
-import React from 'react';
 import MonthlyChart from 'components/monthlyChart/MontlyChart';
+import Nav from 'components/nav/Nav';
+import MyCredit from 'components/myCredit/MyCredit';
+import DonateArtist from 'components/donateArtist/DonateArtist';
 
 function ListPage(props) {
   return (
     <>
-      <MonthlyChart />
+      <Nav />
+      <div className="container" style={{backgroundColor: '#02000E'}}>
+        <MyCredit />
+        <DonateArtist />
+        <MonthlyChart />
+      </div>
     </>
   );
 }
