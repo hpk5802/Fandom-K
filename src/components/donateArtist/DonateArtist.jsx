60e80f1dad36cd7b634ca3b662bc9342f00fe62a
ksm <hpk5802@github.com>
2024-10-31 18:04:21 +0900

    [refactor][강수민] : 코드 리팩토링, 주석 추가

--- src/components/donateArtist/DonateArtist.jsx
+++ src/components/donateArtist/DonateArtist.jsx
@@ -7,22 +7,30 @@ import useWindowSize from 'hooks/useWindowSize';
 
 function DonateArtist() {
   const dispatch = useDispatch();
-  const {donations} = useSelector(state => state.data);
-  const {list, nextCursor} = donations;
-  const [currentPage, setCurrentPage] = useState(0);
+  const {
+    donations: {list, nextCursor},
+  } = useSelector(state => state.data);
+  const [currentPage, setCurrentPage] = useState(0); // 슬라이싱 된 배열의 페이징을 위한 state : 초기값 0
   const isDesktop = useWindowSize();
 
+  /**
+   * 서버에 데이터가 더 있는 경우 데이터를 요청하고 redux store에 추가
+   */
   const fetchMoreDonations = useCallback(() => {
     if (nextCursor) dispatch(getDonations({cursor: nextCursor, pageSize: 4}));
   }, [nextCursor, dispatch]);
 
+  /**
+   * initial load
+   */
   useEffect(() => {
     dispatch(getDonations({pageSize: 4}));
   }, [dispatch]);
 
-  useEffect(() => {
-    setCurrentPage(0);
-  }, [isDesktop]);
+  /**
+   * 페이징 액션(Desktop) -> 터치 액션(Tablet, Mobile) 분기를 자연스럽게 하기 위해 첫 번째 데이터로 강제 이동
+   */
+  useEffect(() => setCurrentPage(0), [isDesktop]);
 
   const totalPages = Math.ceil(list.length / 4);
   const displayedDonations = isDesktop ? list.slice(currentPage * 4, (currentPage + 1) * 4) : list;
--- src/components/donateArtist/SpreadCards.jsx
+++ src/components/donateArtist/SpreadCards.jsx
@@ -2,40 +2,36 @@ import {useCallback, useEffect, useRef} from 'react';
 import CardSection from './CardSection';
 
 function SpreadCards({lists, isDesktop, fetchMoreDonations}) {
-  const endRef = useRef(null);
+  const endRef = useRef(null); // Infinite Scroll 구현을 위한 Ref 객체
 
+  /**
+   * 감시 대상이 화면에 노출되면 서버에 데이터 요청
+   */
   const handleObserver = useCallback(
-    entries => {
-      const [entry] = entries;
-      if (entry.isIntersecting) {
-        fetchMoreDonations();
-      }
+    ([entry]) => {
+      if (entry.isIntersecting) fetchMoreDonations();
     },
     [fetchMoreDonations],
   );
 
+  /**
+   * IntersectionObserver API를 이용해 endRef 객체가 화면에 노출됨을 감지
+   */
   useEffect(() => {
+    if (isDesktop || !endRef.current) return;
+
     const observer = new IntersectionObserver(handleObserver, {
-      root: endRef.current?.parentNode,
-      rootMargin: '0px',
+      root: endRef.current.parentNode,
       threshold: 1.0,
     });
-    const currentEndRef = endRef.current;
 
-    if (!isDesktop && currentEndRef) {
-      observer.observe(currentEndRef);
-    }
+    observer.observe(endRef.current);
 
-    return () => {
-      if (currentEndRef) {
-        observer.unobserve(currentEndRef);
-      }
-    };
+    return () => observer.disconnect();
   }, [handleObserver, isDesktop]);
   return (
     <div className="cards-container">
-      {lists.map(list => {
-        const {id, idol, subtitle, title, receivedDonations, deadline, targetDonation} = list;
+      {lists.map(({id, idol, subtitle, title, receivedDonations, deadline, targetDonation}) => {
         return (
           <CardSection
             key={id}
