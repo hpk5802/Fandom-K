e5690ba78c938d0e23e6ae702eaca24f1fa8d042
myoungsuboh <59433757+myoungsuboh@users.noreply.github.com>
2024-11-02 20:46:49 +0900

    Revert "[feat][오명섭] : gitignore setting"

--- src/index.js
+++ src/index.js
@@ -1,22 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import {BrowserRouter, Route, Routes} from 'react-router-dom';
-import routes from './router';
-import 'style/css/style.css';
-import {Provider} from 'react-redux';
-import store from './redux/store';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import routes from "./router";
+import "style/css/style.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   // <React.StrictMode>
-  <Provider store={store}>
-    <BrowserRouter>
-      <Routes>
-        {routes.map(({path, element}) => (
-          <Route key={path} path={path} element={element} />
-        ))}
-      </Routes>
-    </BrowserRouter>
-  </Provider>,
+  <BrowserRouter>
+    <Routes>
+      {routes.map(({ path, element }) => (
+        <Route key={path} path={path} element={element} />
+      ))}
+    </Routes>
+  </BrowserRouter>
   // {/* </React.StrictMode> */}
 );
