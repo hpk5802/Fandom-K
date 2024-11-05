1447965ba26a946737d0d8f1b54f61bbdcfc9170
ksm <hpk5802@github.com>
2024-11-05 21:10:08 +0900

    [fix] : 잘못 병합된 커밋 수정

--- src/components/donateArtist/DonateArtist.jsx
+++ src/components/donateArtist/DonateArtist.jsx
@@ -25,17 +25,14 @@ function DonateArtist() {
   /**
    * 페이징 액션(Desktop) -> 터치 액션(Tablet, Mobile) 분기를 자연스럽게 하기 위해 첫 번째 데이터로 강제 이동
    */
-  // useEffect(() => setCurrentPage(0), [isDesktop]);
   useEffect(() => setCurrentPage(0), [device]);
 
   useEffect(() => {
     dispatch(resetDonations());
     dispatch(getDonations({pageSize: 4}));
-    // setCurrentPage(0); // 페이지 초기화
   }, [location.pathname, dispatch]);
 
   const totalPages = Math.ceil(list.length / 4);
-  // const displayedDonations = isDesktop ? list.slice(currentPage * 4, (currentPage + 1) * 4) : list;
   const displayedDonations = device === 'desktop' ? list.slice(currentPage * 4, (currentPage + 1) * 4) : list;
   return (
     <Pagination
@@ -48,7 +45,7 @@ function DonateArtist() {
       onPageChange={setCurrentPage}
       fetchMoreData={fetchMoreDonations}
     >
-      <SpreadCards lists={displayedDonations} isDesktop={device} fetchMoreDonations={fetchMoreDonations} />
+      <SpreadCards lists={displayedDonations} device={device} fetchMoreDonations={fetchMoreDonations} />
     </Pagination>
   );
 }
