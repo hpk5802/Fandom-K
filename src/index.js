54fc5c38c4b04d55654964b8131cf764ddfc533e
ksm <hpk5802@github.com>
2024-11-05 16:09:39 +0900

    [fix] : HashRouter로 수정

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import {HashRouter, Route, Routes} from 'react-router-dom';
 import routes from './router';
 import 'style/css/style.css';
 import {Provider} from 'react-redux';
@@ -10,13 +10,13 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   // <React.StrictMode>
   <Provider store={store}>
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         {routes.map(({path, element}) => (
           <Route key={path} path={path} element={element} />
         ))}
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   </Provider>,
   // {/* </React.StrictMode> */}
 );
